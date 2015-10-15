47edda8f0316f4a5510b35a520948b4ff92a5840
Jiri Hnidek <jiri.hnidek@tul.cz>
2015-10-15 21:37:26 +0200

    Added first version of flask application.

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Main file of flask example application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
