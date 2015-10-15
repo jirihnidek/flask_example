45554e496dbf778354b725477e0ec78d34091044
Jiri Hnidek <jiri.hnidek@tul.cz>
2015-10-15 22:57:20 +0200

    Added config and connect to db.

--- main.py
+++ main.py
@@ -2,9 +2,25 @@
 Main file of flask example application.
 """
 
+import sqlite3
+
 from flask import Flask
 
+# configuration
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
 
 @app.route('/')
@@ -13,4 +29,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
+    connect_db()
     app.run()
